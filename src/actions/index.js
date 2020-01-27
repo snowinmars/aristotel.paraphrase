96379b504396c0c98c94f31ae683350696b579ac
snowinmars <marcor@yandex.ru>
2020-01-27 22:03:06 +0300

    Updates

--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-import { HIGHLIGHT_PARAGRAPH_REDUCER_TYPE } from "../constants/types";
-
-export function highlight_paragraph(settings) {
-    return { type: HIGHLIGHT_PARAGRAPH_REDUCER_TYPE, payload: { settings } }
-}
--- src/components/Notes/b1/b1_c10.notes.js
+++
@@ -1,9 +0,0 @@
-const notes = `
-
-[ref:paraphrase 0]
-<sup>(1)</sup> Этот абзац надо бы выкинуть в другую главу, оставив тут только вывод.
-
-`;
-
-export default notes;
-
